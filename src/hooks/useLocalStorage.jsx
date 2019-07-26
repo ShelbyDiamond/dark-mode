ea0a9967249141f256cb14c682920a217783bd92
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-07-25 22:27:05 -0700

    set the stage for the local storage.
    Parsed key and set up stringify values

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -5,9 +5,16 @@ const useLocalstorage = (key, initialValue) {
      const item = window.localStorage.getItem(key)
         if (item) {
             return JSON.parse(item);
+        } else {
+            return initialValue
         }
-    })
+    });
+    const setValue = value => {
+        setStoredValue(value)
+        window.localStorage.setItem(key, JSON.stringify(value))
+    };
+    return [storedValue, setValue]
 }
-
+// useLocalStorage (key, initialValue) ----> [value, setValue]
 //useState(initialValue) =====> [value, setValue]
 export default useLocalStorage
